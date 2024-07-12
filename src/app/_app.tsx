b7c5cd3e36e42d5bc1d565fde07a75bde29ea086
Patrick Jean Meurer <patrickjmeurer@gmail.com>
2024-07-12 16:51:02 -0300

    feat: add GAnalytics

--- src/app/_app.tsx
+++ src/app/_app.tsx
@@ -1,9 +1,11 @@
 import { ThemeProvider } from 'next-themes'
 import { AppProps } from 'next/app'
+import { GoogleAnalytics } from 'nextjs-google-analytics'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider>
+      <GoogleAnalytics trackPageViews />
       <Component {...pageProps} />
     </ThemeProvider>
   )
