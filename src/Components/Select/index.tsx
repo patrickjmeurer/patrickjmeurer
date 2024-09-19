04cde9505bbd870b5b82df18412361e824e842ad
Patrick Jean Meurer <patrickjmeurer@gmail.com>
2024-09-19 18:02:07 -0300

    Project resets
    #86a4zvak3[concluído]

--- src/Components/Select/index.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-import { Select as DefaultSelect, SelectProps } from '@headlessui/react'
-
-const Select: React.FC<SelectProps> = ({ children, ...rest }) => {
-  return <DefaultSelect {...rest}>{children}</DefaultSelect>
-}
-
-export default Select
--- src/Components/ShowMore/index.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-import ShowMoreText, { ReactShowMoreTextProps } from 'react-show-more-text'
-
-const ShowMore: React.FC<ReactShowMoreTextProps> = ({ children, ...rest }) => {
-  return <ShowMoreText {...rest}>{children}</ShowMoreText>
-}
-
-export default ShowMore
